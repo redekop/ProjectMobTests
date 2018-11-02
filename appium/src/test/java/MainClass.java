79104dbf0e1010239042e5636c284ebaf9c4b11f
Kononovich Elena <Elena.Kononovich@life.com.by>
2018-11-02 11:14:07 +0300

    Second test - testGetClassNumber()

--- appium/src/test/java/MainClass.java
+++ appium/src/test/java/MainClass.java
@@ -3,4 +3,15 @@ public class MainClass {
     public int getLocalNumber() {
         return 14;
     }
+
+
+    public Boolean getClassNumber(int number) {
+        if (number <= 45) {
+            System.out.println("Number can't be equal "+number+"");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
--- appium/src/test/java/MainClassTest.java
+++ appium/src/test/java/MainClassTest.java
@@ -3,11 +3,19 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass{
 
-    private int value = 14;
+    private int value;
 
     @Test
     public void testGetLocalNumber() {
+        this.value = 14;
         //assert getLocalNumber() == this.value : ""+getLocalNumber()+" != "+this.value+"";
         Assert.assertEquals( ""+getLocalNumber()+" != "+this.value+"" , getLocalNumber(), this.value);
+    }
+
+
+    @Test
+    public void testGetClassNumber() {
+        this.value = 46;
+        assert getClassNumber(this.value);
     }
 }
