dea3dd38f00d2d1ba21ac6e210447885ec55b644
Kononovich Elena <Elena.Kononovich@life.com.by>
2018-11-02 11:56:12 +0300

    Third test - testGetClassString()

--- appium/src/test/java/MainClass.java
+++ appium/src/test/java/MainClass.java
@@ -1,4 +1,13 @@
+
+
 public class MainClass {
+
+    private String class_string = "Hello, world!";
+
+    public String getClass_string() {
+        return class_string;
+    }
+
 
     public int getLocalNumber() {
         return 14;
--- appium/src/test/java/MainClassTest.java
+++ appium/src/test/java/MainClassTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class MainClassTest extends MainClass{
 
     private int value;
+    private String subString = "hello";
 
     @Test
     public void testGetLocalNumber() {
@@ -17,5 +18,17 @@ public class MainClassTest extends MainClass{
     public void testGetClassNumber() {
         this.value = 46;
         assert getClassNumber(this.value);
+    }
+
+
+    @Test
+    public void testGetClassString() {
+        if (getClass_string().toLowerCase().contains(subString)) {
+            System.out.println("The string="+getClass_string()+" contains a substring="+subString+"" );
+        } else {
+            System.out.println("The string="+getClass_string()+" does not contain a substring="+subString+"" );
+            Assert.fail();
+        }
+
     }
 }
